5896e07fc62a0d40e4bfbade5a5d9af5bb5cd7fe
YousWeb <youssefachkaoukaou@gmail.com>
2026-06-06 17:06:26 +0200

    add microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -75,7 +78,15 @@ export default function RootLayout({
 
   {children}
 
-
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+    {`
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "x2tt7hp1te");
+    `}
+  </Script>
 
 <GoogleAnalytics gaId="G-8E5F4EVSS5" />
 
