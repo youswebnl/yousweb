9ba7cdf4398726241209f02e61e1ed39e706694f
YousWeb <youssefachkaoukaou@gmail.com>
2026-06-09 20:14:53 +0200

    Fix kosten website pagina

---
+++ app/kosten-website-laten-maken/page.tsx
@@ -0,0 +1,7 @@
+export default function KostenWebsiteLatenMakenPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-10">
+      <h1>Kosten website laten maken</h1>
+    </main>
+  );
+}
